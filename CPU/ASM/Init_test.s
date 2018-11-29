4ac24ced48f956ae73fbf7325721596fb7696a10
赵汉卿 <2016011248@secoder.net>
2018-11-29 21:46:53 +0800

    Fix: change lower case to upper case

--- CPU/ASM/Init_test.s
+++ CPU/ASM/Init_test.s
@@ -1,49 +1,50 @@
-nop
-nop
-nop
-nop
+NOP
+NOP
+NOP
+NOP
 
-start:
-  li r3 0
-next_row:
-  li r0 0x001d
-  addiu r3 1
-  next_col:
-    li r1 0x004f
-    ;for row r0, col r1
-    li r2 1
-    addiu r3 0xffff
-    and r2 r3
-    ;r2 = 0 => black
-    ;r2 = 1 => white
+START:
+  LI R3 0
+NEXT_ROW:
+  LI R0 0X001D
+  ADDIU R3 1
+  NEXT_COL:
+    LI R1 0X004F
+    ;FOR ROW R0, COL R1
+    LI R2 1
+    ADDIU R3 0XFFFF
+    AND R2 R3
+    ;R2 = 0 => BLACK
+    ;R2 = 1 => WHITE
 
-    ;GPUpos
-    sll r4 r0 0 ;r4 is pos high 8 digit
-    li r5 0x00ff
-    and r5 r1 ;r5 is pos low 8 digit
-    or r4 r5  ;r4 is pos full
-    ;r6 is 0xbf0a
-    li r6 0x00bf
-    sll r6 r6 0
-    addiu r6 0x0a
-    ;save to 0xbf0a
-    sw r6 r4 0
+    ;GPUPOS
+    SLL R4 R0 0 ;R4 IS POS HIGH 8 DIGIT
+    LI R5 0X00FF
+    AND R5 R1 ;R5 IS POS LOW 8 DIGIT
+    OR R4 R5  ;R4 IS POS FULL
+    ;R6 IS 0XBF0A
+    LI R6 0X00BF
+    SLL R6 R6 0
+    ADDIU R6 0X0A
+    ;SAVE TO 0XBF0A
+    SW R6 R4 0
 
-    ;GPUdata
-    li r6 0x00bf
-    sll r6 r6 0
-    addiu r6 0x0d
-    ;save to 0xbf0d
-    sw r6 r2 0
+    ;GPUDATA
+    LI R6 0X00BF
+    SLL R6 R6 0
+    ADDIU R6 0X0D
+    ;SAVE TO 0XBF0D
+    SW R6 R2 0
 
-    addiu r1 0xffff
-    nop
-    bnez r1 next_col
-    nop
+    ADDIU R1 0XFFFF
+    NOP
+    BNEZ R1 NEXT_COL
+    NOP
 
-  addiu r0 0xffff
-  nop
-  bnez r0 next_row
-  nop
-  b start
-  nop
+  ADDIU R0 0XFFFF
+  NOP
+  BNEZ R0 NEXT_ROW
+  NOP
+  B START
+  NOP
+  
